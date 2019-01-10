f6c5caac0d34a26988b98d534984ffd7270747a3
VADNALWAR, Amit - Dell Team <Amit_VADNALWAR@Dell.com>
2019-01-11 01:22:43 +0530

    First commit

---
+++ build-script.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs-extra');
+const concat = require('concat');    
+
+(async function build() {
+
+    const files =[
+        './dist/ng-element-angular/runtime.js',
+        './dist/ng-element-angular/polyfills.js',
+        './dist/ng-element-angular/main.js'
+    ]
+    
+    await fs.ensureDir('elements')
+    
+    await concat(files, 'elements/ng-greet-element.js')
+    console.info('Angular Elements created successfully!')
+
+})()
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert.component';
+import { CustomerComponent } from './customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
+  entryComponents: [
+    AlertComponent,
+    CustomerComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const alertEl = createCustomElement(AlertComponent, {injector : this.injector});
+    customElements.define('app-alert', alertEl);
+
+    const customerEl = createCustomElement(CustomerComponent, {injector : this.injector});
+    customElements.define('app-customer', customerEl);
+
+  }
+ }
